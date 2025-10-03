6183658ed6f60a5b5db653100387461e55f28bae
serioussmile123 <andreas.riedlinger@victorinox.com>
2025-10-03 12:39:20 +0000

    update

--- 01_restaurant/challenges/01_11_lieblingsgerichtupdate_ch.sql
+++ 01_restaurant/challenges/01_11_lieblingsgerichtupdate_ch.sql
@@ -2,3 +2,22 @@
 
 -- Tragen Sie Cleo Goldwaters Lieblingsgericht 
 -- als "Quinoa Salmon Salad" ein.
+
+SELECT * 
+FROM Kunden 
+WHERE Vorname = 'Cleo' AND Nachname = 'Goldwater';
+
+SElECT *
+FROM Speise
+WHERE Name = 'Quinoa Salmon Salad';
+
+
+UPDATE Kunden
+SET LieblingsGericht = (
+    SELECT SpeiseID
+    FROM Speise
+    WHERE Name = 'Quinoa Salmon Salad'
+)
+WHERE Vorname = 'Cleo' AND Nachname = 'Goldwater';
+
+
